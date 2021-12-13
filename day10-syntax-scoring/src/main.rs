2cb55793ff48bdcc748350e7370e3a15bd4198d0
Maarten Staa <maarten.staa@brightalley.nl>
2021-12-13 17:01:22 +0100

    Add day 10, part 2.

--- day10-syntax-scoring/src/main.rs
+++ day10-syntax-scoring/src/main.rs
@@ -1,10 +1,10 @@
 use std::{iter::Peekable, str::Chars};
 
 fn main() {
-    println!(
-        "{}",
-        first_illegal_character_score_sum(include_str!("input.txt"))
-    );
+    let input = include_str!("input.txt");
+
+    println!("Part 1: {}", first_illegal_character_score_sum(input));
+    println!("Part 2: {}", middle_autocomplete_score(input));
 }
 
 fn first_illegal_character_score_sum(input: &str) -> usize {
@@ -14,6 +14,20 @@ fn first_illegal_character_score_sum(input: &str) -> usize {
         .filter_map(|chunk| first_illegal_character(&chunk))
         .map(illegal_character_score)
         .sum()
+}
+
+fn middle_autocomplete_score(input: &str) -> usize {
+    let mut scores: Vec<_> = input
+        .lines()
+        .map(parse)
+        .filter(|chunk| first_illegal_character(chunk).is_none())
+        .filter_map(|chunk| get_autocomplete(&chunk))
+        .map(|autocomplete| get_autocomplete_score(&autocomplete))
+        .collect();
+
+    scores.sort_unstable();
+
+    scores[scores.len() / 2]
 }
 
 #[derive(Debug, PartialEq)]
@@ -38,17 +52,17 @@ struct Chunk {
     closing_brace: ClosingBrace,
 }
 
-fn first_illegal_character(chunk: &Chunk) -> Option<char> {
-    if let Some(illegal_child_chunk_closing_brace) =
-        chunk.children.iter().find_map(first_illegal_character)
-    {
-        return Some(illegal_child_chunk_closing_brace);
-    }
+fn first_illegal_character(chunk: &[Chunk]) -> Option<char> {
+    chunk.iter().find_map(|chunk| {
+        if let Some(illegal_child_chunk_closing_brace) = first_illegal_character(&chunk.children) {
+            return Some(illegal_child_chunk_closing_brace);
+        }
 
-    match chunk.closing_brace {
-        ClosingBrace::Invalid(c) => Some(c),
-        _ => None,
-    }
+        match chunk.closing_brace {
+            ClosingBrace::Invalid(c) => Some(c),
+            _ => None,
+        }
+    })
 }
 
 fn illegal_character_score(c: char) -> usize {
@@ -61,12 +75,17 @@ fn illegal_character_score(c: char) -> usize {
     }
 }
 
-fn parse(input: &str) -> Chunk {
-    parse_internal(&mut input.chars().peekable())
+fn parse(input: &str) -> Vec<Chunk> {
+    let mut chunks = vec![];
+    let mut chars = input.chars().peekable();
+    while chars.peek().is_some() {
+        chunks.push(parse_internal(&mut chars));
+    }
+
+    chunks
 }
 
 fn parse_internal(input: &mut Peekable<Chars>) -> Chunk {
-    // dbg!("parse_internal", &input);
     let opening_brace = match input.peek() {
         Some('(') => Brace::Parentheses,
         Some('[') => Brace::SquareBrackets,
@@ -74,7 +93,6 @@ fn parse_internal(input: &mut Peekable<Chars>) -> Chunk {
         Some('<') => Brace::AngleBrackets,
         _ => panic!("Invalid opening brace: {}", input.peek().unwrap()),
     };
-    // dbg!(&opening_brace);
 
     // Consume opening brace
     input.next();
@@ -82,7 +100,6 @@ fn parse_internal(input: &mut Peekable<Chars>) -> Chunk {
     let mut children = vec![];
     let mut closing_brace = ClosingBrace::Missing;
     while let Some(c) = input.peek() {
-        // dbg!(c);
         match c {
             ')' => {
                 closing_brace = if opening_brace == Brace::Parentheses {
@@ -130,92 +147,52 @@ fn parse_internal(input: &mut Peekable<Chars>) -> Chunk {
     }
 }
 
-#[test]
-fn test_parse() {
-    assert_eq!(
-        Chunk {
-            opening_brace: Brace::Parentheses,
-            children: vec![],
-            closing_brace: ClosingBrace::Correct
-        },
-        parse("()")
-    );
-    assert_eq!(
-        Chunk {
-            opening_brace: Brace::SquareBrackets,
-            children: vec![Chunk {
-                opening_brace: Brace::Parentheses,
-                children: vec![Chunk {
-                    opening_brace: Brace::SquareBrackets,
-                    children: vec![],
-                    closing_brace: ClosingBrace::Correct
-                }],
-                closing_brace: ClosingBrace::Correct
-            },],
-            closing_brace: ClosingBrace::Invalid(')')
-        },
-        parse("[([]))")
-    );
-    assert_eq!(
-        Chunk {
-            opening_brace: Brace::CurlyBraces,
-            children: vec![
-                Chunk {
-                    opening_brace: Brace::SquareBrackets,
-                    children: vec![],
-                    closing_brace: ClosingBrace::Correct
-                },
-                Chunk {
-                    opening_brace: Brace::Parentheses,
-                    children: vec![Chunk {
-                        opening_brace: Brace::AngleBrackets,
-                        children: vec![],
-                        closing_brace: ClosingBrace::Missing
-                    },],
-                    closing_brace: ClosingBrace::Missing
-                },
-            ],
-            closing_brace: ClosingBrace::Missing
-        },
-        parse("{[](<")
-    );
+fn get_autocomplete(chunk: &[Chunk]) -> Option<String> {
+    // Note that only the last chunk could possibly have missing characters.
+    // Otherwise it could not be the last chunk.
+    match chunk.last() {
+        Some(chunk) => {
+            if chunk.closing_brace != ClosingBrace::Missing {
+                // Then none of the children's closing braces could be missing either
+                return None;
+            }
+
+            Some(format!(
+                "{}{}",
+                get_autocomplete(&chunk.children).unwrap_or_default(),
+                match chunk.closing_brace {
+                    ClosingBrace::Missing => match chunk.opening_brace {
+                        Brace::Parentheses => ")",
+                        Brace::SquareBrackets => "]",
+                        Brace::CurlyBraces => "}",
+                        Brace::AngleBrackets => ">",
+                    },
+                    _ => "",
+                }
+            ))
+        }
+        None => None,
+    }
 }
 
-#[test]
-fn test_first_illegal_character() {
-    // {([(<{}[<>[]}>{[]{[(<()> - Expected ], but found } instead.
-    assert_eq!(
-        Some('}'),
-        first_illegal_character(&parse("{([(<{}[<>[]}>{[]{[(<()>"))
-    );
-    // [[<[([]))<([[{}[[()]]] - Expected ], but found ) instead.
-    assert_eq!(
-        Some(')'),
-        first_illegal_character(&parse("[[<[([]))<([[{}[[()]]]"))
-    );
-    // [{[{({}]{}}([{[{{{}}([] - Expected ), but found ] instead.
-    assert_eq!(
-        Some(']'),
-        first_illegal_character(&parse("[{[{({}]{}}([{[{{{}}([]"))
-    );
-    // [<(<(<(<{}))><([]([]() - Expected >, but found ) instead.
-    assert_eq!(
-        Some(')'),
-        first_illegal_character(&parse("[<(<(<(<{}))><([]([]()"))
-    );
-    // <{([([[(<>()){}]>(<<{{ - Expected ], but found > instead.
-    assert_eq!(
-        Some('>'),
-        first_illegal_character(&parse("<{([([[(<>()){}]>(<<{{"))
-    );
+fn get_autocomplete_score(autocomplete: &str) -> usize {
+    autocomplete.chars().fold(0, |acc, c| {
+        acc * 5
+            + match c {
+                ')' => 1,
+                ']' => 2,
+                '}' => 3,
+                '>' => 4,
+                _ => 0,
+            }
+    })
 }
 
-#[test]
-fn test_part1() {
-    assert_eq!(
-        26397,
-        first_illegal_character_score_sum(
-            "[({(<(())[]>[[{[]{<()<>>
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_INPUT: &str = "[({(<(())[]>[[{[]{<()<>>
 [(()[<>])]({[<{<<[]>>(
 {([(<{}[<>[]}>{[]{[(<()>
 (((({<>}<{<{<>}{[]{[]{}
@@ -224,7 +201,127 @@ fn test_part1() {
 {<[[]]>}<{[{[{[]{()[[[]
 [<(<(<(<{}))><([]([]()
 <{([([[(<>()){}]>(<<{{
-<{([{{}}[<[[[<>{}]]]>[]]"
+<{([{{}}[<[[[<>{}]]]>[]]";
+
+    #[test]
+    fn test_parse() {
+        assert_eq!(
+            vec![Chunk {
+                opening_brace: Brace::Parentheses,
+                children: vec![],
+                closing_brace: ClosingBrace::Correct
+            }],
+            parse("()")
+        );
+        assert_eq!(
+            vec![Chunk {
+                opening_brace: Brace::SquareBrackets,
+                children: vec![Chunk {
+                    opening_brace: Brace::Parentheses,
+                    children: vec![Chunk {
+                        opening_brace: Brace::SquareBrackets,
+                        children: vec![],
+                        closing_brace: ClosingBrace::Correct
+                    }],
+                    closing_brace: ClosingBrace::Correct
+                },],
+                closing_brace: ClosingBrace::Invalid(')')
+            }],
+            parse("[([]))")
+        );
+        assert_eq!(
+            vec![Chunk {
+                opening_brace: Brace::CurlyBraces,
+                children: vec![
+                    Chunk {
+                        opening_brace: Brace::SquareBrackets,
+                        children: vec![],
+                        closing_brace: ClosingBrace::Correct
+                    },
+                    Chunk {
+                        opening_brace: Brace::Parentheses,
+                        children: vec![Chunk {
+                            opening_brace: Brace::AngleBrackets,
+                            children: vec![],
+                            closing_brace: ClosingBrace::Missing
+                        },],
+                        closing_brace: ClosingBrace::Missing
+                    },
+                ],
+                closing_brace: ClosingBrace::Missing
+            }],
+            parse("{[](<")
+        );
+        assert_eq!(
+            vec![
+                Chunk {
+                    opening_brace: Brace::Parentheses,
+                    children: vec![],
+                    closing_brace: ClosingBrace::Correct
+                },
+                Chunk {
+                    opening_brace: Brace::SquareBrackets,
+                    children: vec![Chunk {
+                        opening_brace: Brace::CurlyBraces,
+                        children: vec![],
+                        closing_brace: ClosingBrace::Missing
+                    },],
+                    closing_brace: ClosingBrace::Missing
+                },
+            ],
+            parse("()[{")
+        );
+    }
+
+    #[test]
+    fn test_first_illegal_character() {
+        // {([(<{}[<>[]}>{[]{[(<()> - Expected ], but found } instead.
+        assert_eq!(
+            Some('}'),
+            first_illegal_character(&parse("{([(<{}[<>[]}>{[]{[(<()>"))
+        );
+        // [[<[([]))<([[{}[[()]]] - Expected ], but found ) instead.
+        assert_eq!(
+            Some(')'),
+            first_illegal_character(&parse("[[<[([]))<([[{}[[()]]]"))
+        );
+        // [{[{({}]{}}([{[{{{}}([] - Expected ), but found ] instead.
+        assert_eq!(
+            Some(']'),
+            first_illegal_character(&parse("[{[{({}]{}}([{[{{{}}([]"))
+        );
+        // [<(<(<(<{}))><([]([]() - Expected >, but found ) instead.
+        assert_eq!(
+            Some(')'),
+            first_illegal_character(&parse("[<(<(<(<{}))><([]([]()"))
+        );
+        // <{([([[(<>()){}]>(<<{{ - Expected ], but found > instead.
+        assert_eq!(
+            Some('>'),
+            first_illegal_character(&parse("<{([([[(<>()){}]>(<<{{"))
+        );
+    }
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(26397, first_illegal_character_score_sum(TEST_INPUT))
+    }
+
+    #[test]
+    fn test_get_autocomplete() {
+        assert_eq!(
+            Some(")}>]})".to_owned()),
+            get_autocomplete(&parse("[(()[<>])]({[<{<<[]>>("))
         )
-    )
+    }
+
+    #[test]
+    fn test_get_autocomplete_score() {
+        assert_eq!(5566, get_autocomplete_score(")}>]})"))
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(288957, middle_autocomplete_score(TEST_INPUT));
+    }
 }
